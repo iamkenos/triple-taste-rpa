35ad52bb344c213a3a8d5fd35912dc03ccba735c
iamkenos <alexis.matunog@gmail.com>
2025-02-14 21:46:56 +1100

    refactor: remove redundant prop

--- fixtures/pages/base.page.ts
+++ fixtures/pages/base.page.ts
@@ -3,8 +3,6 @@ import { PageObject, Config } from "@iamkenos/kyoko";
 import type { Parameters } from "./base.steps";
 
 export class BasePage extends PageObject<Parameters> {
-  protected config: Config = global.world.config;
-
   url = "";
   title = "";
 }
