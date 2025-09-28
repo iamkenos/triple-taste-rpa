9175a8a00fa3cc1d0bd5b8738fde06c5627db371
iamkenos <alexis.matunog@gmail.com>
2025-09-28 20:28:57 +1000

    fix: dotenv and video disable

--- cucumber.webhook.js
+++ cucumber.webhook.js
@@ -1,11 +1,13 @@
 const dotenv = require('@dotenvx/dotenvx');
+const fs = require('fs');
+const path = require('path');
 const { configure } = require('@iamkenos/kyoko/config');
 
-dotenv.config()
+const envfile = path.join(__dirname, `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`);
+if (fs.existsSync(envfile)) { dotenv.config({ path: envfile }); }
 exports.default = configure({
   browserOptions: {
     headless: process.env.HEADLESS !== 'false',
-    recordVideo: true,
     browserContextArgs: { timezoneId: 'Asia/Manila' }
   },
   paths: process.env.PATHS ? [...process.env.PATHS] : ['features/webhooks/*.feature'],
